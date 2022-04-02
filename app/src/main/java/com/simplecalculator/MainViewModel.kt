f18d055adea2caf45792f17b4c91db175fefc3f0
AlparslanDev <alp.develioglu@gmail.com>
2022-04-02 16:14:47 +0300

    Format is added

--- app/src/main/java/com/simplecalculator/MainViewModel.kt
+++ app/src/main/java/com/simplecalculator/MainViewModel.kt
@@ -4,6 +4,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import java.math.RoundingMode
+import java.text.DecimalFormat
 
 class MainViewModel : ViewModel() {
 
@@ -16,16 +18,22 @@ class MainViewModel : ViewModel() {
     var muscleWeight by mutableStateOf("")
     var waterWeight by mutableStateOf("")
 
+    private val df = DecimalFormat("#,###.##")
+
+    init {
+        df.roundingMode = RoundingMode.CEILING
+    }
+
     fun calculateFat() {
-        fatWeight = if (fatPercentage != "") (weightNonZero() * fatPercentage.toDouble() / 100).toString() else ""
+        fatWeight = if (fatPercentage != "") df.format(weightNonZero() * fatPercentage.toDouble() / 100) else ""
     }
 
     fun calculateMuscle() {
-        muscleWeight = if (musclePercentage != "") (weightNonZero() * musclePercentage.toDouble() / 100).toString() else ""
+        muscleWeight = if (musclePercentage != "") df.format(weightNonZero() * musclePercentage.toDouble() / 100) else ""
     }
 
     fun calculateWater() {
-        waterWeight = if (waterPercentage != "") (weightNonZero() * waterPercentage.toDouble() / 100).toString() else ""
+        waterWeight = if (waterPercentage != "") df.format(weightNonZero() * waterPercentage.toDouble() / 100) else ""
     }
 
     private fun weightNonZero() = if (weight != "") weight.toDouble() else 1.0
