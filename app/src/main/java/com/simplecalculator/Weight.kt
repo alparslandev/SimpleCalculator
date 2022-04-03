e41cff7d37fdf4b3ada0af7277086b714d5e2988
AlparslanDev <alp.develioglu@gmail.com>
2022-04-03 15:44:22 +0300

    Parcelable is added

--- app/src/main/java/com/simplecalculator/Weight.kt
+++ app/src/main/java/com/simplecalculator/Weight.kt
@@ -1,9 +1,13 @@
 package com.simplecalculator
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Weight(
     val weight: Double,
     val muscle: Double,
     val water: Double,
     val date: String,
     val fat: Double
-)
+): Parcelable
